ba185124d44c756514b22b7f94aa12d2d4d39afd
Thomas F <thomas.fisher@footballpools.com>
2016-11-18 02:58:31 +0000

    [#3] TF : removing unused commented out code

--- app/src/main/java/com/abstractx1/mydiary/record/Stopwatch.java
+++ app/src/main/java/com/abstractx1/mydiary/record/Stopwatch.java
@@ -1,48 +1,8 @@
 package com.abstractx1.mydiary.record;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 /**
  * Created by tfisher on 17/11/2016.
  */
-
-//public class Stopwatch {
-//    private Timer timer;
-//    private int elapsedTime;
-//
-//    public Stopwatch() {
-//        reset();
-//    }
-//
-//    private void reset() {
-//        this.timer = new Timer();
-//        this.elapsedTime = 0;
-//    }
-//
-//    public void start() {
-//        reset();
-//        timer.schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-//                incrementRecordingDuration();
-//            }
-//
-//        }, 0, 1000);
-//    }
-//
-//    public void stop() {
-//        timer.cancel();
-//    }
-//
-//    public int getElapsedTime() {
-//        return elapsedTime;
-//    }
-//
-//    private void incrementRecordingDuration() {
-//        this.elapsedTime += 1;
-//    }
-//}
 
 public class Stopwatch {
     private long startTime;

4c6ef63ea696220643c6e93f429987ea19a1456c
Thomas F <thomas.fisher@footballpools.com>
2016-12-22 18:37:15 +0000

    [#0] TF : cleanup duplicate class

--- app/src/main/java/com/abstractx1/mydiary/camera/CameraHandler.java
+++ app/src/main/java/com/abstractx1/mydiary/camera/CameraHandler.java
@@ -1,19 +1,22 @@
-package com.abstractx1.mydiary.camera;
+package com.abstractx1.mydiary;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 
-import com.abstractx1.mydiary.Utilities;
-
 import java.io.File;
 import java.io.IOException;
+
 /**
  * Created by tfisher on 14/11/2016.
  */
+
 /*
-*/
+
+ */
+
 public class CameraHandler {
     //http://stackoverflow.com/questions/7720383/camera-intent-not-saving-photo
     public static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -21,10 +24,12 @@ public class CameraHandler {
     private static final String CAPTURE_IMAGE_FILE_PROVIDER = "com.abstractx1.fileprovider";
     private final Activity activity;
     public String imagePath;
+
     public CameraHandler(Activity activity, String imagePath) {
         this.activity = activity;
         this.imagePath = imagePath;
     }
+
     public void dispatchTakePictureIntent() throws IOException {
         if (hasCameraApp()) {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -36,10 +41,12 @@ public class CameraHandler {
             throw new RuntimeException("No camera apps installed");
         }
     }
+
     private boolean hasCameraApp() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         return takePictureIntent.resolveActivity(activity.getPackageManager()) != null;
     }
+
     public String getImagePath() {
         return imagePath;
     }
